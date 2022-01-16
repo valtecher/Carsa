51bcd438a76aafdafeb887745c5c5acc1d86d787
JolyMolyf <vs2001dor@gmail.com>
2022-01-16 12:54:47 +0100

    moew d

--- config/db.ts
+++ config/db.ts
@@ -5,11 +5,12 @@ const dev = {
     password: process.env.PG_PASSWORD, 
     database: process.env.PG_DB,
     host: process.env.PG_HOST,
-    dialect: process.env.PG_DIALECT
+    dialect: 'postgres'
 }
 const prod = {
     connectionString: process.env.DATABASE_URL
 }
+
 
 module.exports = {
     "development": dev,
