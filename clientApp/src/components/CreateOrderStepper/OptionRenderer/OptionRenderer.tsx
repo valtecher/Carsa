594d23cb9c7c5693e849bcf893e4fe5c3ea75e36
JolyMolyf <vs2001dor@gmail.com>
2022-01-16 12:19:33 +0100

    ьщку ашч

--- clientApp/src/components/CreateOrderStepper/OptionRenderer/OptionRenderer.tsx
+++ clientApp/src/components/CreateOrderStepper/OptionRenderer/OptionRenderer.tsx
@@ -1,38 +1,38 @@
 import React, { useState } from 'react';
-import AcceptTerms from '../AcceptTerms/AcceptTerms';
-import ChoosePackagePackage from '../Step1/StepperChoosePackage'
-import ConfigurationCreator from '../linkcreation/ConfigurationCreator';
-import { CarConfigurationType } from '../../../interfaces/models/carConfiguration';
-import { Package } from '../../../interfaces/models/package';
-import StripeContainer from '../paymentmethod/StripeContainer';
+// import AcceptTerms from '../AcceptTerms/AcceptTerms';
+// import ChoosePackagePackage from '../Step1/StepperChoosePackage'
+// import ConfigurationCreator from '../linkcreation/ConfigurationCreator';
+// import { CarConfigurationType } from '../../../interfaces/models/carConfiguration';
+// import { Package } from '../../../interfaces/models/package';
+// import StripeContainer from '../paymentmethod/StripeContainer';
 // import { SelectedOptionType } from '../../../pages/DashBoard/Order/CreateOrder/createOrder';
 
 
-export const handleRender = (activeStep:number, next: (answer: CarConfigurationType | Package  | null) => any, orderCreated: any) => {
+export const handleRender = () => {
 
     
-  switch(activeStep){
-    case 0:
-      return(
-        <ChoosePackagePackage next={next}/>
-      )
-    case 1: 
-        return(
-          <ConfigurationCreator type={orderCreated.package!.type}  next={next}></ConfigurationCreator>
-        )
-    case 2:
-      return(
-          <AcceptTerms next={next}/>
-      )
-    case 3: 
-        return(  
-          <StripeContainer selectedOptions={orderCreated} />
-        )
-    default:
-      return(
-        <div>
+  // switch(activeStep){
+  //   case 0:
+  //     return(
+  //       <ChoosePackagePackage next={next}/>
+  //     )
+  //   case 1: 
+  //       return(
+  //         <ConfigurationCreator type={orderCreated.package!.type}  next={next}></ConfigurationCreator>
+  //       )
+  //   case 2:
+  //     return(
+  //         <AcceptTerms next={next}/>
+  //     )
+  //   case 3: 
+  //       return(  
+  //         <StripeContainer selectedOptions={orderCreated} />
+  //       )
+  //   default:
+  //     return(
+  //       <div>
           
-        </div>
-      )
-  }
+  //       </div>
+  //     )
+  // }
 }
