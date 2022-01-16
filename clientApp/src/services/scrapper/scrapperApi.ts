96130592822c2b5736bec0d27a8317ef5c1a13ce
JolyMolyf <vs2001dor@gmail.com>
2022-01-16 17:40:06 +0100

    timeout changed

--- clientApp/src/services/scrapper/scrapperApi.ts
+++ clientApp/src/services/scrapper/scrapperApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const getScrappedModel = async (link:string, handleLoading?:any) => {
-  return await axios.get(`${process.env.REACT_APP_API_URL}/scraper`, { timeout: 10000, headers: {link}}).then((res:any) => {
+  return await axios.get(`${process.env.REACT_APP_API_URL}/scraper`, { timeout: 100000, headers: {link}}).then((res:any) => {
     handleLoading(false);
     return res.data;
   })
