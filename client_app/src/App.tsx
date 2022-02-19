6ae4e3ef7fb03a828d735b595d45fe56833c16f4
JolyMolyf <vs2001dor@gmail.com>
2022-02-19 11:59:27 +0100

    About page ready

--- client_app/src/App.tsx
+++ client_app/src/App.tsx
@@ -7,12 +7,14 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import About from './pages/About/About';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/home' element={<HomePage/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
         <Route path="*" element={<Navigate to="/home" />} />
       </Routes>
     </Router>
---
+++ client_app/src/pages/About/About.tsx
@@ -0,0 +1,21 @@
+import './About.scss'
+import Header from '../../components/header/Header';
+
+const About = () => {
+  return(
+    <div className='about'>
+      <Header/>
+      <div className='about-header'>
+          About Us
+      </div>
+      <div className='about-body'>
+         <p>CARSA introduces experiences of mobile valeting services where you can recieve an entire package of full or mini valets, whether it's to protect, polish or wash your car.</p>
+          <p>Business began to thrive in April 2021, and has not stopped since. With the dedication, passion and motivation for cars we take pride in every single job we carry out.</p>
+          <p>We aim to help car lovers and enthusiasts to keep their cars up to standards and well maintained. If you feel like you have the same passion about looking after your car, you are in the right place.</p>
+          <p>A lot of people make big investments in their cars, so why not keep it squeaky clean and treat your car to an occasional valet with Auto Freak Ltd to keep up the good quality of your car.</p>
+      </div>
+    </div>
+  )  
+}
+
+export default About;
