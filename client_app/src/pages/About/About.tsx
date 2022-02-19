4d5518061105fcd8335a2e17611956adcb95cf11
JolyMolyf <vs2001dor@gmail.com>
2022-02-19 12:07:37 +0100

    Add image for about page

--- client_app/src/pages/About/About.tsx
+++ client_app/src/pages/About/About.tsx
@@ -1,4 +1,5 @@
 import './About.scss'
+import AstonPhoto from '../../images/HomePage/aston_martin_PNG9.png'
 import Header from '../../components/header/Header';
 
 const About = () => {
@@ -13,6 +14,9 @@ const About = () => {
           <p>Business began to thrive in April 2021, and has not stopped since. With the dedication, passion and motivation for cars we take pride in every single job we carry out.</p>
           <p>We aim to help car lovers and enthusiasts to keep their cars up to standards and well maintained. If you feel like you have the same passion about looking after your car, you are in the right place.</p>
           <p>A lot of people make big investments in their cars, so why not keep it squeaky clean and treat your car to an occasional valet with Auto Freak Ltd to keep up the good quality of your car.</p>
+      </div>
+      <div className='about-image'>
+        <img src={AstonPhoto}></img>
       </div>
     </div>
   )  
