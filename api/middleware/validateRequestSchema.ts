4af6dc3d8bf99c158dfe3fb00377214e30c00263
Denys Kryzhanivskyi <denys.kryzhanivskyi@valtech.com>
2022-07-04 14:34:48 +0200

    Created CRUD endpoints for configurations

--- api/middleware/validateRequestSchema.ts
+++ api/middleware/validateRequestSchema.ts
@@ -3,11 +3,11 @@ import { validationResult } from 'express-validator';
 import { StatusCodes } from 'http-status-codes';
 
 export const validateRequestSchema = (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req);
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return res.status(StatusCodes.BAD_REQUEST).json({ errors: errors.array() });
-    }
+  if (!errors.isEmpty()) {
+    return res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({ errors: errors.array() });
+  }
 
-    next();
-}
+  next();
+};
