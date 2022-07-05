960b473d0ee100c2791a2838c225459764dba9e0
Denys Kryzhanivskyi <denys.kryzhanivskyi@valtech.com>
2022-07-05 12:09:48 +0200

    Updated some car endpoints

--- api/routes/cars.ts
+++ api/routes/cars.ts
@@ -5,38 +5,14 @@ import { validateRequestSchema } from '../middleware/validateRequestSchema';
 
 const router = Router();
 
-router.get(
-    '/list',
-    carsController.getAllCars
-);
+router.get('/list', carsController.getAllCars);
 
-router.get(
-    '/:carId',
-    validateCarId,
-    validateRequestSchema,
-    carsController.getCarById
-);
+router.get('/:carId', validateCarId, validateRequestSchema, carsController.getCarById);
 
-router.post(
-    '/',
-    validateCreateCar,
-    validateRequestSchema,
-    carsController.createCar
-);
+router.post('/', validateCreateCar, validateRequestSchema, carsController.createCar);
 
-router.put(
-    '/:carId',
-    validateCarId,
-    validateUpdateCar,
-    validateRequestSchema,
-    carsController.updateCarById
-);
+router.put('/:carId', validateCarId, validateUpdateCar, validateRequestSchema, carsController.updateCarById);
 
-router.delete(
-    '/:carId',
-    validateCarId,
-    validateRequestSchema,
-    carsController.deleteCarById
-);
+router.delete('/:carId', validateCarId, validateRequestSchema, carsController.deleteCarById);
 
 export default router;
